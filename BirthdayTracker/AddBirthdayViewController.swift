f65cac42731b3d8ecf42c96c8975f56afab5f9f8
latyshev <latyshev831@gmail.com>
2019-07-12 22:14:19 +0300

    init commit

---
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Евгений Латышев on 7/12/19.
+//  Copyright © 2019 Evgeny Latyshev. All rights reserved.
+//
+
+import UIKit
+
+protocol AddBirthdayViewControllerDelegate {
+  func addBirthdayViewController(_ addBirthdayViewController: AddBirthdayViewController, didAddBirthday birthday: Birthday)
+}
+
+class AddBirthdayViewController: UIViewController {
+  @IBOutlet var firstNameTextField: UITextField!
+  @IBOutlet var lastNameTextField: UITextField!
+  @IBOutlet var birthdatePicker: UIDatePicker!
+  
+  var delegate: AddBirthdayViewControllerDelegate?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    birthdatePicker.maximumDate = Date()
+  }
+  
+  @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+    let firstName = firstNameTextField.text ?? ""
+    let lastName = lastNameTextField.text ?? ""
+    let birthdate = birthdatePicker.date
+    let newBirthday = Birthday(firstName: firstName, lastName: lastName, birthdate: birthdate)
+    
+    delegate?.addBirthdayViewController(self, didAddBirthday: newBirthday)
+    dismiss(animated: true, completion: nil)
+  }
+  
+  @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+    dismiss(animated: true, completion: nil)
+  }
+}
+
